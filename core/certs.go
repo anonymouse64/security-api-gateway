baa4dae61e79c88b250c69cf39f9fd08c6172777
Ian Johnson <ian.johnson@canonical.com>
2019-06-20 00:25:52 -0500

    use json instead of form
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- core/certs.go
+++ core/certs.go
@@ -39,7 +39,7 @@ func loadKongCerts(config *tomlConfig, url string, secretBaseURL string, c *http
 	lc.Info(fmt.Sprintf("posting body: %+v", body))
 
 	lc.Info("Trying to upload cert to proxy server.")
-	req, err := sling.New().Base(url).Post(CertificatesPath).BodyForm(body).Request()
+	req, err := sling.New().Base(url).Post(CertificatesPath).BodyJSON(body).Request()
 	resp, err := c.Do(req)
 	if err != nil {
 		lc.Error("Failed to upload cert to proxy server with error %s", err.Error())
